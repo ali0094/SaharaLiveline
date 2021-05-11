22cbcea27065bfa42d4b86e2d36be743227a3d9d
ali <muhammad.ali0094@outlook.com>
2021-05-12 02:03:18 +0400

    Splash activity work

---
+++ app/src/main/java/com/saharaliveline/ui/MatchActivity.kt
@@ -0,0 +1,12 @@
+package com.saharaliveline.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.saharaliveline.R
+
+class MatchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_match)
+    }
+}
