42e1767756ae1ef7e4818ece71f2f29986c175af
ali <muhammad.ali0094@outlook.com>
2021-05-13 00:43:13 +0400

    added SplashActivity.kt

---
+++ app/src/main/java/com/saharaliveline/ScoreApplication.kt
@@ -0,0 +1,9 @@
+package com.saharaliveline
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ScoreApplication : Application() {
+
+}
--- app/src/main/java/com/saharaliveline/ui/SplashActivity.kt
+++ app/src/main/java/com/saharaliveline/ui/SplashActivity.kt
@@ -1,15 +1,39 @@
 package com.saharaliveline.ui
 
+import android.content.Intent
 import android.os.Bundle
 import com.saharaliveline.databinding.ActivitySplashBinding
+import kotlinx.coroutines.*
 
+@ExperimentalCoroutinesApi
 class SplashActivity : BaseActivity<ActivitySplashBinding>() {
+
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(mViewBinding.root)
+
+        activityScope.launch {
+            delay(SPLASH_DELAY)
+            val intent = Intent(this@SplashActivity, MatchActivity::class.java)
+            startActivity(intent)
+            finishAffinity()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        activityScope.cancel()
     }
 
     override fun getViewBinding(): ActivitySplashBinding =
         ActivitySplashBinding.inflate(layoutInflater)
+
+    companion object {
+        private const val SPLASH_DELAY = 2000L
+    }
+
+
 }
